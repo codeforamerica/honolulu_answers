8b8d8511780db76fde4e59574f3a29d08ea5222e
Diana Tran <diana.tran@me.com>
2012-05-01 15:00:06 -0700

    fixed stylesheet

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(:version => 20120501191040) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "role"
     t.boolean  "admin"
   end
 
