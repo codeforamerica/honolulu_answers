6f5f90c1d466b1b5bc874a5d89b19b095108cc30
Mick Thompson <dthompson@gmail.com>
2012-05-23 14:00:56 -1000

    auto reindex on save

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
+include ActionView::Helpers::SanitizeHelper
+
 class Article < ActiveRecord::Base
   # has_and_belongs_to_many :locations
   belongs_to :contact
+  after_commit :index_article
+
   def self.search(search)
     if search
       where('title ILIKE ? OR content ILIKE ?', "%#{search}%", "%#{search}%")
@@ -21,5 +25,14 @@ class Article < ActiveRecord::Base
   def allContent()
     [self.title, self.content].join(" ")
   end
+
+  private
+    def index_article
+      client = IndexTank::Client.new(ENV['SEARCHIFY_API_URL'])
+      index = client.indexes("hnlanswers-"+ENV['RAILS_ENV'])
+
+      index.document(self.id.to_s).add({ :text => strip_tags(self.content), :title => self.title, :tags => self.tags.to_s, :preview => self.preview.to_s })
+      print "index updated"
+    end
 
 end
