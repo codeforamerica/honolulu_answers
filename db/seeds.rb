5a666c26f05b057ea7ed57fccd0bd37a6533521d
Mick Thompson <dthompson@gmail.com>
2012-05-31 12:28:48 -1000

    add some data via seeds.db as example data to work with in development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+puts 'SETTING UP DEFAULT USER LOGIN'
+print "Enter admin email: "
+admin_email = STDIN.gets.to_s
+
+user = User.create! :email => admin_email, :password => 'password123', :password_confirmation => 'password123', :admin => true
+puts 'New user created: ' << user.email
+
+Article.create([{title: 'Example 1', content: "Some example content goes here"}, {title: 'Example 2', content: "more example"}] )
+puts 'Created example articles'
