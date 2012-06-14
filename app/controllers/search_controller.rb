b634750460012cb775efec8e89782ba25bd67cd7
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-06-14 01:04:28 +0100

    Search is now much more lenient to natural language queries.  The search query is first stripped of words defined in lib assets eng_stop.csv, before being sent over to Searchify where stemming allows for pluralization and similar variations. One problem is the way this has been implemented leads to a pretty big slowdown on searches - its still pretty quick for individual users but there must be a more intelligent way of subtracting the excluded words from the query

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,21 @@ class SearchController < ApplicationController
     client = IndexTank::Client.new(ENV['SEARCHIFY_API_URL'])
     index = client.indexes("hnlanswers-"+ENV['RAILS_ENV'])
     
+    query = params[:q]
+    ## Pre-process the search query for natural language searches
 
-    @results = index.search("("+params[:q]+") OR (title:"+params[:q]+ ") OR (tags:"+params[:q]+")",
+    # Stop list of common English words
+    eng_stop_list = CSV.read( "#{Rails.root.to_s}/lib/assets/eng_stop.csv" )
+
+    # Remove these words from the query
+    query = query.split - eng_stop_list.flatten
+
+    # Default behaviour is AND; this changed it to OR
+    query = query.join " "
+
+    logger.info "Search Query: #{query}"
+
+    @results = index.search("(#{query}) OR (title: #{query}) OR (tags:#{query})",
                             :fetch => 'title,timestamp,preview', 
                             :snippet => 'text')
     render :json => @results
