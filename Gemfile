d4702cda4bae031d49e1a2d7b36f7b090b158880
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-06-19 01:39:21 +0100

    deleted an unneeded gem from the gemfile, lets see if this works

--- Gemfile
+++ Gemfile
@@ -45,7 +45,7 @@ group :assets do
 
 end
 
-gem 'jquery-rails', '>= 1.0.12'
+# gem 'jquery-rails', '>= 1.0.12'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/application.rb
+++ config/application.rb
@@ -2,16 +2,16 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# if defined?(Bundler)
-#   # If you precompile assets before deploying to production, use this line
-#   # Bundler.require(*Rails.groups(:assets => %w(development test)))
-#   # If you want your assets lazily compiled in production, use this line
-#   Bundler.require(:default, :assets, Rails.env)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
 # end
 
-require 'devise'
-require 'rails_admin'
-require 'tanker'
+# require 'devise'
+# require 'rails_admin'
+# require 'tanker'
 
 module Honoluluanswers
   class Application < Rails::Application
