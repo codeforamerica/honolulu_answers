04214946d87ac4d7653623727fb20697cf241ae7
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-06-19 01:25:18 +0100

    put back the lines of code that are in fact needed - this is where the problem lies though

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# if defined?(Bundler)
-#   # If you precompile assets before deploying to production, use this line
-#   Bundler.require(*Rails.groups(:assets => %w(development test)))
-#   # If you want your assets lazily compiled in production, use this line
-#   # Bundler.require(:default, :assets, Rails.env)
-# end
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # If you want your assets lazily compiled in production, use this line
+  Bundler.require(:default, :assets, Rails.env)
+end
 
-require 'devise'
-require 'rails_admin'
+# require 'devise'
+# require 'rails_admin'
 
 module Honoluluanswers
   class Application < Rails::Application
