81e20b73af7ba76d34de8ba1ac6b31a5a3910c45
Amir Reavis-Bey <amirbey@gmail.com>
2012-07-02 17:05:27 -0400

    added role attributes admin,editor, moderator accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :is_published#:admin
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :is_admin,:is_moderator,:is_editor#:admin
 
 end
