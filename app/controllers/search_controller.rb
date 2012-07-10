217cccc6f08b2947d3deb957b44334b0a2e3ed2c
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-10 17:57:48 +0100

    improved the logging of search queries, will be more useful now that we drain logs to papertrail

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,9 @@ class SearchController < ApplicationController
     @query = query
     logger.info "  Search query: #{query}"
 
+    # remove puntuation and plurals.
     query = query.downcase.gsub(/[^\w]/, ' ').gsub(/ . /, ' ')
-    logger.info "  Query cleaned: #{query}"
+    logger.info "  Query no-punct: #{query}"
 
     query = Article.remove_stop_words query
     logger.info "  Removed stop words: #{query}"
@@ -20,8 +21,10 @@ class SearchController < ApplicationController
 
     logger.info "  Results found: #{@results.size}"
 
-    # Might be useful for fine-tuning search
-    logger.debug( "#{request.env['REMOTE_ADDR']},#{@query},#{@results.size}" )
+    # Log the search results
+    if Rails.env === 'production'
+      logger.debug( "search-request: IP:#{request.env['REMOTE_ADDR']}, QUERY:#{@query}, QUERY_CORRECTED:#{@query_corrected}, FIRST_RESULT:#{@results.first.title}, RESULTS_N:#{@results.size}" )
+    end
 
   end
   
