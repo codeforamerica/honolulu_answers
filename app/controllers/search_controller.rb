5afbfb16f4e3792112504ab606117a5cab2b0a85
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-10 19:15:23 +0100

    search now works with the query expansion module.  I imagine search will now be overly permissive - unable to test properly until I have the articles loaded into my local database.  in order to do this, i will need to write the rake database seeds file, story ID #32465277 (since this branches schema differs from anything on heroku, taps will not work).  I think this finishes [#33422541] - as soon as the seeds file is created i can deliver.  next steps are: thoroughly testing the search, experiment with boosting certain attributes.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,31 +1,37 @@
 class SearchController < ApplicationController
+   include RailsNlp::BigHugeThesaurus
 
   def index  
     query =  params[:query] 
+    return redirect_to articles_path if params[:query].nil?
     @query = query
-    logger.info "  Search query: #{query}"
 
     # remove puntuation and plurals.
     query = query.downcase.gsub(/[^\w]/, ' ').gsub(/ . /, ' ')
-    logger.info "  Query no-punct: #{query}"
 
+    # remove stop words
     query = Article.remove_stop_words query
-    logger.info "  Removed stop words: #{query}"
 
-    @query_corrected = Article.spell_check query
-    logger.info "  Spell check: #{@query}"
+    # expand the query
+    stems,metaphones,synonyms = [[],[],[]]
+    query.split.each do |term|
+      stems << Text::PorterStemming.stem(term)
+      synonyms << RailsNlp::BigHugeThesaurus.synonyms(term) 
+      metaphones << Text::Metaphone.double_metaphone(term)
+    end
+    stems = 'stems:' + stems.flatten.join(' OR stems:')
+    metaphones = 'metaphones:' + metaphones.flatten.compact.join(' OR metaphones:')
+    synonyms = 'synonyms:"' + synonyms.flatten.first(3).join( '" OR synonyms:"') + '"'
 
-    @results = Article.search_tank( @query_corrected, 
+    query_final = "#{query} OR #{stems} OR #{metaphones} OR #{synonyms}"
+    @results = Article.search_tank( query_final, 
                               :fetch => [:title, :timestamp, :preview],
                               :snippets => [:content] )
-
-    logger.info "  Results found: #{@results.size}"
+    # spell check the query
+    @query_corrected = Article.spell_check query
 
     # Log the search results
-    if Rails.env === 'production'
-      logger.debug( "search-request: IP:#{request.env['REMOTE_ADDR']}, QUERY:#{@query}, QUERY_CORRECTED:#{@query_corrected}, FIRST_RESULT:#{@results.first.title}, RESULTS_N:#{@results.size}" )
-    end
-
+    logger.debug( "search-request: IP:#{request.env['REMOTE_ADDR']}, params[:query]:#{query}, QUERY:#{query_final}, FIRST_RESULT:#{@results.first.title unless @results.empty?}, RESULTS_N:#{@results.size}" )
   end
   
 end
