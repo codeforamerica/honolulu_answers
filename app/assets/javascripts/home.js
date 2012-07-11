ed67096ec27e945283c61e5813805ac5b1d6c145
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-11 03:38:19 +0100

    empty search in the URL now redirecrs to all articles.  empty search in the search box now does nothing [#31040283] [#31323457]

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,6 +12,7 @@ $(function(){
     $("form#search:not(.noanimation)").submit(function(e){       
         e.preventDefault();
         var query = $("#query").val().replace(/\"/g,"");
+        if(query == '') { return; }
         searchControl.startSearch(query);
         history.pushState({"query":query}, "Searching for - "+query, "/search?q="+encodeURIComponent(query));
     })
