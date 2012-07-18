0dcc7a667acb1863ed90df79f7ce6c9dff19a4f5
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-18 01:51:35 +0100

    reverting back to non-precompiled assets (again)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Honoluluanswers::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false # TEMPORARILY TRUE PENDING FIX OF HEROKU ASSET PRECOMPILATION ISSUE
+  config.assets.compile = true # TEMPORARILY TRUE PENDING FIX OF HEROKU ASSET PRECOMPILATION ISSUE
 
   # Generate digests for assets URLs
   config.assets.digest = true
