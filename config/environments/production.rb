75a2a85826e13b03dc185ff94cefb07d29de83c8
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-18 01:33:08 +0100

    configuring production and staging environments to use Memcached to store static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Honoluluanswers::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -44,6 +44,12 @@ Honoluluanswers::Application.configure do
 
   # Use a different cache store in production
   config.cache_store = :dalli_store, { :compress => true }
+
+  config.action_dispatch.rack_cache = {
+    :metastore    => Dalli::Client.new,
+    :entitystore  => 'file:tmp/cache/rack/body',
+    :allow_reload => false
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -9,7 +9,7 @@ Honoluluanswers::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,6 +19,8 @@ Honoluluanswers::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.static_cache_control = "public, max-age=2592000"
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
@@ -41,6 +43,12 @@ Honoluluanswers::Application.configure do
 
   # Use a different cache store in production
   config.cache_store = :dalli_store, { :compress => true }
+
+  config.action_dispatch.rack_cache = {
+    :metastore    => Dalli::Client.new,
+    :entitystore  => 'file:tmp/cache/rack/body',
+    :allow_reload => false
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
