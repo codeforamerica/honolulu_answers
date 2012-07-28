cf7f6e20b802b7093979d343fedbe08b1fec030e
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-07-28 03:40:54 +0100

    specifying amazon s3 as the storage for paperclip

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,7 +16,13 @@ class Article < ActiveRecord::Base
   has_many :wordcounts
   has_many :keywords, :through => :wordcounts
 
-  has_attached_file :author_pic, :styles => { :thumb => "100x100" }, :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+  has_attached_file :author_pic, 
+                    :bucket => 'hnlanswers-production',
+                    :styles => { :thumb => "100x100" }, 
+                    :s3_credentials => {
+                      :access_key_id => ENV['S3_KEY'],
+                      :secret_access_key => ENV['S3_SECRET']
+                    }
 
   validates_presence_of :access_count
 
