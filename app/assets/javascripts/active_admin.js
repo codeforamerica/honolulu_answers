b92c37be460c0e0de1dd0a9264df8850bddf95cc
Joey Cody <jcody@umail.ucsb.edu>
2012-08-02 14:38:18 -0700

    added in tinymce editor to active admin config

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,12 @@
 //= require active_admin/base
+
+tinyMCE.init({
+  mode : "textareas",
+  theme : "advanced",
+  theme_advanced_buttons1 : "bold, italic, underline, strikethrough, |, bullist, numlist, blockquote, |, undo, redo, |, link, unlink, code",
+  theme_advanced_buttons2 : "",
+  theme_advanced_buttons3 : "",
+  theme_advanced_toolbar_location : "top",
+  theme_advanced_toolbar_align : "center",
+  theme_advanced_resizing : false
+});
