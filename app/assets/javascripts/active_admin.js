114c729d9bd642228b4c44a905fead2d6337c9b2
Nick Doiron <ndoiron@mapmeld.com>
2012-09-11 16:28:53 -0700

    Don't try to init tinyMCE if it's undefined

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,9 +1,11 @@
 //= require active_admin/base
 
 $(document).ready(function() {
-  tinyMCE.init({
+  if(typeof tinyMCE !=== 'undefined'){
+    tinyMCE.init({
           mode: 'specific_textareas',
           editor_selector : 'editor',
           theme: 'advanced'
-  });
+    });
+  }
 });
