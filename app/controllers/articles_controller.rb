966c6c609b0940b83396d364e650c7e923c8015e
Philip Hale <p.hale.09@aberdeen.ac.uk>
2012-09-11 14:11:02 +0100

    add redirect to old /articles/ paths to avoid duplication

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,26 +19,9 @@ class ArticlesController < ApplicationController
   # GET /articles/1
   # GET /articles/1.json
   def show
-    return render(:template => 'articles/missing') unless Article.exists? params[:id]
-
+    # redirect to the new paths (such as /quick_answers/1 instead of /articles/1)
     @article = Article.find(params[:id])
-    # redirection of old permalinks
-    if request.path != article_path( @article )
-      logger.info "Old permalink: #{request.path}"
-      return redirect_to @article, status: :moved_permanently
-    end
-
-    # @article.increment! :access_count
-    # @article.category.increment!(:access_count) if @article.category   
-
-    # @content_html = BlueCloth.new(@article.content).to_html
-    @content_html = @article.content
-    @bodyclass = "results"
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @article }
-    end    
+    return redirect_to @article, status: :moved_permanently
   end
   
 
