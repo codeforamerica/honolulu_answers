8da12bc8d0d0597ed1f5269db2275a61bee034ae
Nick Doiron <ndoiron@mapmeld.com>
2012-09-11 16:51:16 -0700

    Remove extra =

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,7 +1,7 @@
 //= require active_admin/base
 
 $(document).ready(function() {
-  if(typeof tinyMCE !=== 'undefined'){
+  if(typeof tinyMCE !== 'undefined'){
     tinyMCE.init({
           mode: 'specific_textareas',
           editor_selector : 'editor',
