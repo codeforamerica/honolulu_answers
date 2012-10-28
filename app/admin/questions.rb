c4a19060b2d887296ddd425c6ffc675151c8ad40
Jonathan Pichot <jon.pichot@gmail.com>
2012-10-27 20:28:37 -0400

    begin on listing the open questions

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -11,4 +11,16 @@ ActiveAdmin.register Question do
     column "Created", :created_at
     default_actions # Add show, edit, delete column
   end
+
+  form do |f|   # create/edit user form
+    f.inputs "Question Details" do
+      f.input :question
+      f.input :context
+      f.input :email
+      f.input :name
+      f.input :location
+      f.input :status
+    end
+    f.buttons
+  end
 end
