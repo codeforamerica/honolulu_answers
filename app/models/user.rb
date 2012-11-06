ba159fa22655889b426899b6fa6354d49cbfd32a
Joel Mahoney <joelmahoney@gmail.com>
2012-11-06 04:34:59 -0800

    Add Department and associate with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable, :registerable,
+  belongs_to :department
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
 
