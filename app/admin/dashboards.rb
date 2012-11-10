c397bf44fabd9026108cea3586cc36d9c990839c
Terry Miyajima <tmiyajima@honolulu.gov>
2012-11-09 16:13:58 -1000

    Active Admin Updates
    - Update CSS for Status column.
    - Add priority attribute to order sections.

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -1,6 +1,6 @@
 ActiveAdmin::Dashboards.build do
-  
-  section("Recent Articles", :if => proc {current_user.is_admin? || current_user.is_editor? }) do
+
+  section("Recent Articles", :if => proc {current_user.is_admin? || current_user.is_editor? }, :priority => 1) do
     table_for Article.order("created_at DESC").limit(5) do
       column "Article Title", :title do |article|
         link_to article.title, [:admin, article]
@@ -14,21 +14,21 @@ ActiveAdmin::Dashboards.build do
     end
     strong { link_to "View All Articles", admin_quick_answers_path }
   end
-  
-  section("Your Articles", :if => proc {current_user.is_writer? }) do
+
+  section("Your Articles", :if => proc {current_user.is_writer? }, :priority => 2) do
     table_for current_user.articles.order("created_at DESC") do
       column "Article Title", :title do |article|
         link_to article.title, [:admin, article]
       end
       column "Author", :user do |article|
         article.user.try(:email)
-      end      
+      end
       column "Status", :status
       column "Date Created", :created_at
       column "Date Updated", :updated_at
     end
   end
-  
+
   section("Users", :if => proc{ current_user.is_admin? }) do
     table_for User.order("created_at DESC").limit(5) do
       column "User", :email do |user|
