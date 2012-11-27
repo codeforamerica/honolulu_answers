a54eb4a2464a0dc514c57805be4fd225cf182829
Nick Doiron <ndoiron@mapmeld.com>
2012-11-26 21:19:20 -0500

    admin panel labeling changes

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -10,4 +10,44 @@ $(document).ready(function(){
 	$("a[href $= '#']").click(function(e){
 		$("a[href $= '#']").next().css({ "display": "block" });
 	});
+	
+	/* if( $("#page_title").text() == "Dashboard"){
+		var headers = $("thead th");
+		for(var h=0;h<headers.length;h++){
+			if( $($("thead th")[h]).text() == "Author" ){
+				$($("thead th")[h]).text("Writer");
+				break;
+			}
+		}
+	}
+
+	if( $("#page_title").text() == "Quick Answers"){
+		var headers = $("thead th");
+		for(var h=0;h<headers.length;h++){
+			if( $($("thead th")[h]).text() == "Writer link" ){
+				$($("thead th")[h]).text("Write-a-thon Author Link");
+				break;
+			}
+		}
+	}
+
+	if( $("#page_title").text() == "Web Services"){
+		var headers = $("thead th");
+		for(var h=0;h<headers.length;h++){
+			if( $($("thead th")[h]).text() == "Writer link" ){
+				$($("thead th")[h]).text("Write-a-thon Author Link");
+				break;
+			}
+		}
+	}
+
+	if( $("#page_title").text() == "Guides"){
+		var headers = $("thead th");
+		for(var h=0;h<headers.length;h++){
+			if( $($("thead th")[h]).text() == "Writer link" ){
+				$($("thead th")[h]).text("Write-a-thon Author Link");
+				break;
+			}
+		}
+	} */
 });
