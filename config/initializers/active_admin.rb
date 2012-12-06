69c0c3174eb3f20d0aef5a8f21fa03057cfe46e1
Terry Miyajima <tmiyajima@honolulu.gov>
2012-12-06 09:36:30 -1000

    Update Active Admin Site Title and Devise Reply Email Address

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Honolulu Answers Admin"
+  config.site_title = "Honolulu Answers CMS"
   
   # config.namespace :admin do |admin|
   #   admin.site_title = "Honolulu Answers Admin"
