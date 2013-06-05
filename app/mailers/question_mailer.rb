fc97c27adc4ec095f7ca95fded0623aa66a12e8a
Philip Hale <p.hale.09@aberdeen.ac.uk>
2013-06-05 03:50:46 +0100

    change citizen question form to use env var for contact emaili and from email

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,8 +1,8 @@
 class QuestionMailer < ActionMailer::Base
-  default from: "feedback@wmgreenguide.com"
+  default from: "feedback@answers.honolulu.gov"
 
   def submitted_question(question)
     @question = Question.find(question.id)
-    mail(:to => 'contact@wmgreenguide.com', :subject => "[WMGG] New question by #{question.name}")
+    mail(:to => ENV['CONTACT_EMAIL'], :subject => "[#{ENV['SITE_NAME']}] New question by #{question.name}")
   end
 end
