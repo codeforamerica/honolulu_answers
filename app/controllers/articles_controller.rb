2f8f2a05e8645b64dc65c41faab513b08ebc6bfc
Philip Hale <phil989@gmail.com>
2013-09-01 23:14:13 +0100

    [#18] Legacy articles URLs show "missing" partial when article not found

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,14 +13,14 @@ class ArticlesController < ApplicationController
     end
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
+  # redirect to the new paths (such as /quick_answers/1 instead of /articles/1)
   def show
-    # redirect to the new paths (such as /quick_answers/1 instead of /articles/1)
+    return render(:template => 'articles/missing') unless Article.exists? params[:id]
     @article = Article.find(params[:id])
     return redirect_to @article, status: :moved_permanently
   end
 
+  # This is what you would expect to be called CategoriesController#show
   def article_type
     @article_type = params[:content_type]
     # convert paramaterized url
