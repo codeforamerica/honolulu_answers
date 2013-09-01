b44a1c16ad8c825d969847e6f647fb1d36205c56
Philip Hale <phil989@gmail.com>
2013-09-01 23:35:52 +0100

    [#18] Use https for article contributor images from S3

--- app/models/article.rb
+++ app/models/article.rb
@@ -30,6 +30,7 @@ class Article < ActiveRecord::Base
       :access_key_id => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET']
     },
+    :s3_protocol => :https,
     :path => "/:style/:id/:filename",
     :styles => { :thumb => "100x100" }
 
