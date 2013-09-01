fd4874c94becb7ac7ddad54d4972bb4105011ed2
Philip Hale <phil989@gmail.com>
2013-09-01 01:58:12 +0100

    [#18] Tabs instead of spaces for GuidesController

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,39 +1,34 @@
 class GuidesController < ApplicationController
-	# caches_page :show
-	def show
-		return render(:template => 'articles/missing') unless Guide.exists? params[:id]
+  def show
+    return render(:template => 'articles/missing') unless Guide.exists? params[:id]
 
-		@article = Guide.find(params[:id])
+    @article = Guide.find(params[:id])
 
-		return render(:template => 'articles/missing') unless @article.published?
-	    #redirection of old permalinks
-	    if request.path != guide_path( @article )
-	      logger.info "Old permalink: #{request.path}"
-	      return redirect_to @article, status: :moved_permanently
-	    end
+    return render(:template => 'articles/missing') unless @article.published?
+    #redirection of old permalinks
+    if request.path != guide_path( @article )
+      logger.info "Old permalink: #{request.path}"
+      return redirect_to @article, status: :moved_permanently
+    end
 
-	    @article.delay.increment! :access_count
-	    @article.delay.category.increment!(:access_count) if @article.category   
+    @article.delay.increment! :access_count
+    @article.delay.category.increment!(:access_count) if @article.category
 
-	    unless @article.render_markdown
-	      @content_html = @article.content
-  	      hr = /<hr( \/)?>/
-          if @content_html.match hr
-            @content_html.gsub!(hr,"</div>")
-            @content_html = "<div class='quick_top'>" + @content_html
-          end
-	      render :show_html and return
-	    end
-    	#content = @article.render_markdown ? @article.content_md : @article.content
-	    #@content_html = BlueCloth.new(content).to_html
+    unless @article.render_markdown
+      @content_html = @article.content
+        hr = /<hr( \/)?>/
+        if @content_html.match hr
+          @content_html.gsub!(hr,"</div>")
+          @content_html = "<div class='quick_top'>" + @content_html
+        end
+      render :show_html and return
+    end
 
-        # Add support for quick-top in markdown
+    @content_main = @article.md_to_html( @article.content )
 
-   		@content_main = @article.md_to_html( @article.content )
-
-	    respond_to do |format|
-	      format.html # show.html.erb
-	      format.json { render json: @article }
-	    end 
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @article }
+    end
   end
 end
