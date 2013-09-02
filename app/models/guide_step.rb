13462212a91a133edf548bab93132a1c13aa7765
Philip Hale <phil989@gmail.com>
2013-09-02 18:18:37 +0100

    [#18] Workaround to make GuideSteps indexable.
    Delegates some methods to the Guide association, wraps another.

--- app/models/guide_step.rb
+++ app/models/guide_step.rb
@@ -2,4 +2,10 @@ class GuideStep < ActiveRecord::Base
   include TankerArticleDefaults
   belongs_to :guide, :class_name => 'Guide', :foreign_key => 'article_id'
   attr_accessible :article_id, :title, :content, :preview, :step
+
+  delegate :category, :tags, :keywords, :to => :guide
+
+  def content_md
+    content
+  end
 end
