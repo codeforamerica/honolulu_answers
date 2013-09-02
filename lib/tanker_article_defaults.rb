63dd5e677b946317fe165ad315e52296de95868c
Philip Hale <phil989@gmail.com>
2013-09-02 15:56:19 +0100

    [#18] Spaces instead of tabs for tanker_article_defaults

--- lib/tanker_article_defaults.rb
+++ lib/tanker_article_defaults.rb
@@ -4,27 +4,27 @@ module TankerArticleDefaults
 
     index = 'hnlgovanswers-development'
     index = 'hnlgovanswers' if Rails.env === 'production'
-    
+
     base.tankit index, :as => 'Article' do
       indexes :title
-	    indexes :content_md
-	    indexes :category, :category => true
-	    indexes :tags
-	    indexes :preview
+      indexes :content_md
+      indexes :category, :category => true
+      indexes :tags
+      indexes :preview
 
-	    # NLP
-	    indexes :metaphones do
-	      keywords.map { |kw| kw.metaphone }
-	    end
-	    indexes :synonyms do
-	      keywords.map { |kw| kw.synonyms.first(5) }
-	    end
-	    indexes :keywords do
-	      keywords.map { |kw| kw.name }
-	    end
-	    indexes :stems do
-	      keywords.map { |kw| kw.stem }
-	    end
+      # NLP
+      indexes :metaphones do
+        keywords.map { |kw| kw.metaphone }
+      end
+      indexes :synonyms do
+        keywords.map { |kw| kw.synonyms.first(5) }
+      end
+      indexes :keywords do
+        keywords.map { |kw| kw.name }
+      end
+      indexes :stems do
+        keywords.map { |kw| kw.stem }
+      end
     end
   end
 end
