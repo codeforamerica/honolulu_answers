f861f12ec108e8490638e31b3ed9e26b52a4df02
Philip Hale <phil989@gmail.com>
2013-09-07 17:40:19 +0100

    [#47] Ignore #access_count when detecting for changes to Article.
    We dont' want to create new versions every time the article is accessed.

--- app/models/article.rb
+++ app/models/article.rb
@@ -64,7 +64,8 @@ class Article < ActiveRecord::Base
 
   before_validation :set_access_count_if_nil
 
-  has_paper_trail :class_name => 'ArticleVersion'
+  has_paper_trail :class_name => 'ArticleVersion',
+    :ignore => [:access_count]
 
   STATUS = [
     DRAFT = "Draft",
