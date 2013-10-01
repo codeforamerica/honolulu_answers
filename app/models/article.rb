e6ed949f7e13ffba30e1195d904983b9ddea35d3
Philip Hale <phil989@gmail.com>
2013-10-01 12:45:21 +0100

    [#44] Remove content field from quick answers, web services, guides.
    But not guide steps, which use content for the markdown.

--- app/models/article.rb
+++ app/models/article.rb
@@ -38,12 +38,12 @@ class Article < ActiveRecord::Base
 
   validates_presence_of :access_count
 
-  attr_accessible :title, :content, :content_main,
-    :content_main_extra, :content_need_to_know, :preview,
-    :contact_id, :tags, :is_published, :slugs, :category_id, :updated_at,
-    :created_at, :author_pic, :author_pic_file_name, :author_pic_content_type,
-    :author_pic_file_size, :author_pic_updated_at, :author_name, :author_link,
-    :type, :service_url, :user_id, :published, :pending_review
+  attr_accessible :title, :content_main, :content_main_extra,
+    :content_need_to_know, :preview, :contact_id, :tags, :is_published, :slugs,
+    :category_id, :updated_at, :created_at, :author_pic, :author_pic_file_name,
+    :author_pic_content_type, :author_pic_file_size, :author_pic_updated_at,
+    :author_name, :author_link, :type, :service_url, :user_id, :published,
+    :pending_review
 
   # Tanker callbacks to update the search index
   after_save :update_tank_indexes
