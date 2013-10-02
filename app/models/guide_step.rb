f6878647e547301f9a5b711ab0a241352d8ea58f
Philip Hale <phil989@gmail.com>
2013-10-02 17:05:35 +0100

    [#77] Add tanker callbacks to guide_step

--- app/models/guide_step.rb
+++ app/models/guide_step.rb
@@ -1,11 +1,15 @@
 class GuideStep < ActiveRecord::Base
   include TankerArticleDefaults
-  belongs_to :guide, :class_name => 'Guide', :foreign_key => 'article_id'
+  belongs_to :guide, :class_name => 'Article', :foreign_key => 'article_id'
   attr_accessible :article_id, :title, :content, :preview, :step
 
   delegate :category, :tags, :keywords, :to => :guide
 
+  after_save { guide.update_tank_indexes }
+  after_destroy { guide.delete_tank_indexes }
+
   def content_md
     content
   end
+
 end
