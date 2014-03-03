7517c2a5a7f0d34ed5d893b27f1c5da53d31df03
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-03 12:29:03 -0600

    added bundle execs

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -4,6 +4,6 @@ ruby infrastructure/bin/create_database.rb
 mv /tmp/database.yml config/database.yml
 gem install bundler
 bundle install
-rake db:schema:load
-rake db:seed
+bundle exec rake db:schema:load
+bundle exec rake db:seed
 ruby infrastructure/bin/create_honolulu_answers_stack.rb --db `cat /tmp/rds_instance`
