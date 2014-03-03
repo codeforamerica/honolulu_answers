0c30941ea890aa1eff54602106cd4ece4388c174
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-03 01:05:16 -0500

    Added a config.yml instead of using ENV

--- config/initializers/tanker.rb
+++ config/initializers/tanker.rb
@@ -1,2 +1,2 @@
-Honoluluanswers::Application.config.index_tank_url = ENV['SEARCHIFY_API_URL']
+Honoluluanswers::Application.config.index_tank_url = AppConfig[:searchify_api_url]
 Honoluluanswers::Application.config.tanker_pagination_backend = :kaminari
