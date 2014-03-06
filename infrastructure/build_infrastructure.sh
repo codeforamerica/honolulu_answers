ad8aa90629e445a6723fc3c9c4f28f0e33a37a0c
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-06 15:46:03 -0500

    Added debug and bundle install

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -e
 # aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat infrastructure/config/honolulu.template`"  --disable-rollback
+echo "========= Gem list pre bundle install ========="
+gem list
+bundle install
 
+echo "========= Gem list post bundle install ========="
+gem list
 ruby infrastructure/bin/create_cloudformation_stack.rb
