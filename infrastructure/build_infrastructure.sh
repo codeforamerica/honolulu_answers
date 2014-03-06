b96b19818397218d9b36c73cc30ce83352c3eaeb
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-06 15:40:12 -0600

    cleaning up build script.

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 # aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat infrastructure/config/honolulu.template`"  --disable-rollback
-echo "========= Gem list pre bundle install ========="
-gem list
+gem install bundle
 bundle install
-
-echo "========= Gem list post bundle install ========="
-gem list
 ruby infrastructure/bin/create_cloudformation_stack.rb
