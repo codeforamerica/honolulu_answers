8053d80331b4549292caed34c8f41e464ee91875
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-05 21:53:45 -0600

    updated path

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
- aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat honolulu.template`"  --disable-rollback 
+ aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat infrastructure/config/honolulu.template`"  --disable-rollback 
