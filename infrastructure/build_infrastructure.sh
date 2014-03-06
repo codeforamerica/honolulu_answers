baffdd3e1c5582ddf4085473fcd159a9c20f2f0b
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-06 16:08:55 -0600

    install bundler and bundle install first thing.

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
+gem install bundler
+bundle install
 export RAILS_ENV="development"
 ruby infrastructure/bin/create_database.rb
 mv /tmp/database.yml config/database.yml
-gem install bundler
-bundle install
 bundle exec rake db:schema:load
 bundle exec rake db:seed
 ruby infrastructure/bin/create_honolulu_answers_stack.rb --db `cat /tmp/rds_instance`
