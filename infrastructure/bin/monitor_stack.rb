bac7e83bafa1c13d6cfd4c8567f2e130b6a5add5
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-06 17:39:17 -0600

    added scripted to monitor cfn stack

---
+++ infrastructure/bin/monitor_stack.rb
@@ -0,0 +1,51 @@
+require 'trollop'
+require 'aws-sdk-core'
+
+STDOUT.sync = true
+
+
+# using trollop to do command line options
+opts = Trollop::options do
+  opt :region, 'The AWS region to use', :type => String, :default => "us-west-2"
+  opt :stack, 'name of the stack to monitor', :type => String, :required => true
+end
+
+
+# we set up a CLoudFormation stack, and we need to know if it's done yet. These are the statuses indicating "not done yet"
+PROGRESS_STATUSES = [
+  "CREATE_IN_PROGRESS",
+  "ROLLBACK_IN_PROGRESS",
+  "DELETE_IN_PROGRESS",
+  "UPDATE_IN_PROGRESS",
+  "UPDATE_COMPLETE_CLEANUP_IN_PROGRESS",
+  "UPDATE_ROLLBACK_IN_PROGRESS",
+  "UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS" ]
+
+# checks to see if the cfn stack is done yet
+def stack_in_progress cfn_stack_name
+  status = @cfn.describe_stacks(stack_name: cfn_stack_name).stacks.first[:stack_status]
+  return PROGRESS_STATUSES.include? status
+end
+
+def print_and_flush(str)
+  print str
+  $stdout.flush
+end
+
+# create a cfn stack with all the resources the opsworks stack will need
+@cfn = Aws::CloudFormation.new region: "us-west-2"
+
+cfn_stack_name = opts[:stack]
+
+print_and_flush "waiting for stack #{cfn_stack_name} to come up..."
+while (stack_in_progress cfn_stack_name)
+  print_and_flush "."
+  sleep 30
+end
+puts
+
+# get the resource names out of the cfn stack so we can pass themto opsworks
+resources = {}
+@cfn.describe_stacks(stack_name: cfn_stack_name).stacks.first[:outputs].each do |output|
+  resources[output[:output_key]] = output[:output_value]
+end
