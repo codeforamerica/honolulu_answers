3b610a0eed0b7782ef258111f9b844794ec839dd
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-05 21:47:47 -0600

    updating build script to use cloudformation template

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,9 +1,2 @@
 #!/bin/bash -e
-export RAILS_ENV="development"
-ruby infrastructure/bin/create_database.rb
-mv /tmp/database.yml config/database.yml
-gem install bundler
-bundle install
-bundle exec rake db:schema:load
-bundle exec rake db:seed
-ruby infrastructure/bin/create_honolulu_answers_stack.rb --db `cat /tmp/rds_instance`
+ aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat honolulu.template`"  --disable-rollback 
