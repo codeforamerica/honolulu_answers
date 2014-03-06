43b2639130f3028c135536d0a773abd5dc95ab71
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-06 14:03:37 -0500

    Added path from root dir

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 # aws cloudformation create-stack --stack-name Honolulu-`date +%Y%m%d%H%M%S` --template-body "`cat infrastructure/config/honolulu.template`"  --disable-rollback
 
-ruby bin/create_cloudformation_stack.rb
+ruby infrastructure/bin/create_cloudformation_stack.rb
