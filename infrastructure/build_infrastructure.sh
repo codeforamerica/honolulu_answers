07510433216c1e63226a7c3075820379ad441905
Brian Jakovich <brian.jakovich@stelligent.com>
2014-03-06 23:52:48 -0500

    Changed vars to bash vars

--- infrastructure/build_infrastructure.sh
+++ infrastructure/build_infrastructure.sh
@@ -7,7 +7,7 @@ mv /tmp/database.yml config/database.yml
 bundle exec rake db:schema:load
 bundle exec rake db:seed
 
-echo ENV["AWS_ACCT_NUMBER"]
-echo ENV["AWS_SECRET_ACCESS_KEY"]
-echo ENV["AWS_ACCESS_KEY"]
-ruby infrastructure/bin/create_honolulu_answers_stack.rb --db `cat /tmp/rds_instance` --accountnumber ENV["AWS_ACCT_NUMBER"]
+echo $AWS_ACCT_NUMBER
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_ACCESS_KEY
+ruby infrastructure/bin/create_honolulu_answers_stack.rb --db `cat /tmp/rds_instance` --accountnumber $AWS_ACCT_NUMBER
