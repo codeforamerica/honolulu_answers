4c47c24a296c9d82053fedc93243f791dc13f499
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-03-10 20:27:48 -0500

    adding build-and-deploy.sh

---
+++ infrastructure/build-and-deploy.sh
@@ -0,0 +1,6 @@
+gem install trollop 
+gem install aws-sdk-core --pre
+export stack_name=HonoluluAnswers-`date +%Y%m%d%H%M%S`
+aws cloudformation create-stack --stack-name $stack_name --template-body "`cat infrastructure/config/honolulu.template`" --region ${region}  --disable-rollback --capabilities="CAPABILITY_IAM"
+ruby infrastructure/bin/monitor_stack.rb  --stack $stack_name
+echo stack_name=$stack_name > stackname.txt
